fecf62957e07c2f1d23a8005b4647128fd4a494e
Denis <kalaladm@mail.uc.edu>
2025-04-08 10:42:52 -0400

    Added the project I added it to the wrong repository.

---
+++ 03_MAUITestMVVM/03_MAUITestMVVM-master/ATest1/ViewModels/CallsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ATest1.Models;
+using PropertyChanged;
+using System.Collections.ObjectModel;
+
+namespace ATest1.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class CallsViewModel
+    {
+
+        public ObservableCollection<ContactModel> Contacts { get; } = new();
+        public Command LoadMoreCommand { get; set; }
+        public CallsViewModel()
+        {
+            LoadMoreCommand = new(LoadMore);
+            //new Thread(GenerateContacts).Start();
+        }
+
+        void LoadMore()
+        {
+            GenerateContacts();
+        }
+
+        void GenerateContacts()
+        {
+            var list = ContactListGenerator.GetContacts(10);
+            for (int i = 0; i < list.Count(); i++)
+            {
+                Contacts.Add(list.ElementAtOrDefault(i));
+            }
+        }
+    }
+}
